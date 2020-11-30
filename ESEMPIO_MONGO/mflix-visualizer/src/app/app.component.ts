215b9c1592b1f1e8bd2039ed9d16df5177cad5b1
CrovaxVI <scarpante.niccolo@iisgalvanimi.edu.it>
2020-11-30 10:44:40 +0000

    prep verifica

---
+++ ESEMPIO_MONGO/mflix-visualizer/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mflix-visualizer';
+  results : Object[];
+  obs : Observable<object>;
+  constructor(private http : HttpClient, private sanitizer: DomSanitizer){}
+
+  load10Movies()
+  {
+    this.obs = this.http.get('https://3000-fd4df663-b957-4bfc-ae8d-576b15c54490.ws-eu01.gitpod.io/movies');
+    this.obs.subscribe(this.getData);
+  }
+
+  getData = (data) => {
+    this.results = data;
+  }
+
+  photoURL(urltoSanitize) {
+    console.log(urltoSanitize);
+    if (urltoSanitize==undefined){
+        return false ;
+    }
+
+    return this.sanitizer.bypassSecurityTrustUrl(urltoSanitize);
+}
+}
